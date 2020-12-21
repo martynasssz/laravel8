9a3157fb0ab904530687cdb12c8e7a5b9a0c6ff5
Martynas <martynasssk@gmail.com>
2020-12-21 08:20:12 +0200

    single action controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +27,8 @@ Route::get('/', [HomeController::class, 'home'])
     ->name('home.index');
 Route::get('/contact', [HomeController::class,'contact'])
     ->name('home.contact');
+
+Route::get('/single', AboutController::class); //single action conntroller route    
 
     $posts = [
         1 => [
