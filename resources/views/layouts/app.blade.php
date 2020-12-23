43de88240a48e5c0790210dffa205c57d9b7232c
Martynas <martynasssk@gmail.com>
2020-12-23 19:44:11 +0200

    Includin Assets in Views (add css and javascrip in app.blade.php)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+    
+<head> 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Laravel App - @yield('title')</title>
-
-<head> 
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer > </script>
+    <title> Laravel App - @yield('title')</title>   
 
 </head>
 
