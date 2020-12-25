78af1450fac7d21e36396a5ba2f14bfe0327f47e
Martynas <martynasssk@gmail.com>
2020-12-25 13:05:41 +0200

    Styling Post List Page IMPORTANT TO REPEAT!

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,15 +1,11 @@
-{{--  @break($key == 2)  //stop loop at key2   --}}
-{{--  @continue ($key == 1) //skyp first element and continue with secod element    --}}
-@if($loop->even)
-<div>{{ $key }}. {{ $post['title'] }}</div>
-@else
-<div style="background-color: silver">{{ $key }}. {{ $post->title }}</div>
-@endif
+{{--  leave only one style  --}}
+<h3><a href="{{ route('posts.edit', ['post' => $post->id]) }}">{{ $post->title }}</a></h3>
 
-<div>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+<div class="bm-3">
+    <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
+    <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
            @csrf
            @method('DELETE')
-           <input type="submit" value="Delete!">
+           <input type="submit" value="Delete!" class="btn btn-primary">
     </form>
 </div>
