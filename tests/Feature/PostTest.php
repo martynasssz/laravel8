cdc02920fb134a0588d77f28edfa525947d97d2b
Martynas <martynasssk@gmail.com>
2020-12-26 13:18:08 +0200

    Testing database interactions

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost; //new model added
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase; //it lets recreate database stucture by running all migrations on each single test run
+    
+    public function testNoBlogPostsWhenNothingInDatabase() //act part
+    {
+        $response = $this->get('/posts');
+
+        $response->assertSeeText('No post found!'); //assert part
+    }
+
+    public function testSee1BlogPostWhenThereIs1()
+    {
+        //Arrange (preparation data for testing, saving a new model inside database)
+        $post = new BlogPost(); //use App\Models\BlogPost added
+        $post->title = 'New title';
+        $post->content = 'Content of blog post';
+        $post->save();
+
+        //Act
+        $response = $this->get('/posts');
+
+        //Assert 
+        $response->assertSeeText('New title');
+
+        //chech if there is a record with specific atribute in database
+        $this->assertDatabaseHas('blog_posts', [
+            'title' => 'New title'
+        ]);
+
+
+
+    }
+
+}
