766a46f17acb39d83fa20afbd9bb36c27f0bfc59
Martynas <martynasssk@gmail.com>
2020-12-26 12:24:54 +0200

    Testing configuration and environment. Creating database connection in memory

--- config/database.php
+++ config/database.php
@@ -43,6 +43,11 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'sqlite_testing' => [
+            'driver' => 'sqlite',            
+            'database' => ':memory:',   //database shoul be created in memory         
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
