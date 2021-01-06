812e0ba03e1be285a0bd36e58a13d754c50bc22e
Martynas <martynasssk@gmail.com>
2021-01-06 19:27:21 +0200

    Model factory introduction. IMPORTANT! For laravel 8 solved errors, see in comments

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\BlogPost; //new model added
+use App\Models\Comment;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use Tests\TestCase;
@@ -18,7 +19,7 @@ class PostTest extends TestCase
         $response->assertSeeText('No post found!'); //assert part
     }
 
-    public function testSee1BlogPostWhenThereIs1()
+    public function testSee1BlogPostWhenThereIs1WihtNoComments()
     {
         //Arrange (preparation data for testing, saving a new model inside database)
         // $post = new BlogPost(); //use App\Models\BlogPost added
@@ -38,6 +39,22 @@ class PostTest extends TestCase
         $this->assertDatabaseHas('blog_posts', [
             'title' => 'New title'
         ]);
+    }
+
+    public function testSee1BlogPostWithComments()
+    {
+        //Arrange
+        $post=$this->createDummyBlogPost();
+
+        Comment::factory()->count(4)->create([
+            'blog_post_id' => $post->id
+        ]);
+                       
+        $response = $this->get('/posts');
+
+        $response ->assertSeeText('4 comments');
+
+
     }
 
     public function testStoreValid()
